b93344b0d3ee907a4618e566fb9c331ce3b565a2
happy-sonday <mrsmiler1106@gmail.com>
2022-01-11 16:21:16 +0900

    first commit

---
+++ src/components/Video.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      <h1>비디오</h1>
+      {/* <VideoFooter /> */}
+      {/* <VideoSidebar />*/}
+    </div>
+  );
+}
+
+export default Video;
